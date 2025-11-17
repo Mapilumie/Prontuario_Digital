11d9c8daff50088a631f674c7352a7c116464d2d
Fátima Lôbo <leitelobofatimavitoria2004@gmail.com>
2025-11-17 04:02:33 -0300

    Criar classe para realizar a conexão com o banco de dados

---
+++ src/main/java/br/com/prontuario/db/ConnectionFactory.java
@@ -0,0 +1,18 @@
+package br.com.prontuario.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    private static final String USERNAME = "seu_usuario_aqui";
+
+    private static final String PASSWORD = "sua_senha_aqui";
+
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/prontuario";
+
+    public Connection getConnection() throws SQLException {
+        Connection conexao = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+        return conexao;
+    }
+}
