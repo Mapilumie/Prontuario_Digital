66088631ae382eca6fb3be6af974b2daea3cbb7e
Fátima Lôbo <leitelobofatimavitoria2004@gmail.com>
2025-11-23 19:05:43 -0300

    Adicionar bloco finally a classe que testa a conexão com o banco de dados

--- src/main/java/br/com/prontuario/db/TesteConexao.java
+++ src/main/java/br/com/prontuario/db/TesteConexao.java
@@ -5,12 +5,20 @@ import java.sql.SQLException;
 
 public class TesteConexao {
     public static void main(String[] args) {
+        Connection conexao = null;
         try {
-            Connection conexao = new ConnectionFactory().getConnection();
+            conexao = new ConnectionFactory().getConnection();
             System.out.println("Conexão estabelecida com sucesso!");
-            conexao.close();
         } catch (SQLException e) {
             System.err.println("Erro ao conectar com o banco de dados: " + e.getMessage());
+        } finally {
+            try {
+                if (conexao != null) {
+                    conexao.close();
+                }
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar Connection: " + e.getMessage());
+            }
         }
     }
 }
